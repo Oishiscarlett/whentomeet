d9658b73bdb2e921aa766b053c4a541eb73ae1b7
ueiaa <70129098+ueiaa@users.noreply.github.com>
2020-10-14 22:51:05 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Faqs from '../views/Faqs.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/faqs', component: Faqs}
+  
 ]
 
 const router = new VueRouter({
