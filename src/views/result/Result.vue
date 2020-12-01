da0ce7db09ef23c40f77e2192b681714699576cc
ueiaa <ueiaa0712@163.com>
2020-12-01 18:22:10 +0800

    12.01结果页面bug修复

--- src/views/result/Result.vue
+++ src/views/result/Result.vue
@@ -469,11 +469,16 @@ export default {
               formatapi.slotMaxTime = showHours[1] + ":00"
               let highlightHours = res.data.data.calendar.highlightHours.split(",")
               formatapi.businessHours = {
-                daysOfWeek: [ 1, 2, 3, 5, 6, 0],
+                //daysOfWeek: [ 1, 2, 3, 5, 6, 0],
                 startTime: highlightHours[0],
                 endTime: highlightHours[1], 
               }
-              formatapi.hiddenDays = [],
+              let hiddenDay = res.data.data.calendar.hiddenDays.split(",")
+              formatapi.hiddenDays = []
+              for(var i = 0; i<hiddenDay.length; i++){
+                  formatapi.hiddenDays.push(Number(hiddenDay[i]));
+                  
+              }
               formatapi.validRange = {
                 start: res.data.data.calendar.startTime,
                 end: res.data.data.calendar.endTime
