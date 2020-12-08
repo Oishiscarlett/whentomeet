1d37cda6c9cb49615558de51c87cb1f766463580
oishi <oishi4869@163.com>
2020-12-08 15:59:07 +0800

    导航栏布局修复

--- src/components/common/tabbar/TabBar.vue
+++ src/components/common/tabbar/TabBar.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="tab-bar">
-    <div id="web-icon"><slot name="web-icon"></slot></div>
-    <div id="tab-bar-items"><slot></slot></div>
+  <div id="tab-bar-box">
+    <div id="tab-bar">
+      <div id="web-icon"><slot name="web-icon"></slot></div>
+      <div id="tab-bar-items"><slot></slot></div>
+    </div>
   </div>
 </template>
 
@@ -12,18 +14,23 @@ export default {
 </script>
 
 <style scoped>
-  #tab-bar {
+  #tab-bar-box {
     width: 100%;
     height: 70px;
-    display: flex;
-    align-items: center;
-
+    
     border-bottom: 1px solid #409EFF;
     box-shadow: 1px 1px 3px rgb(167, 205, 255);
 
     position: fixed;
     top: 0;
     z-index: 100;
+  }
+
+  #tab-bar {
+    width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
   }
 
   #web-icon {
